0c57ca793d0cc4096fe7d6066c06487693c89872
goalTosin <gunsin.goal@gmail.com>
2026-06-18 20:23:17 +0100

    Unrefined Idea

---
+++ index.js
@@ -0,0 +1,13 @@
+function basicEval(board) {}
+
+function evaluate(board, turn, options) {
+  if (isForcingMove && options.calculateForcing) {
+    return getForcingMoves()
+      .map((move) => {
+        return evaluate(makeMove(board, move), 1 - turn, options);
+      })
+      .reduce((a, b) => (a[turn] > b[turn] ? a : b));
+  } else {
+    return basicEval(board)
+  }
+}
